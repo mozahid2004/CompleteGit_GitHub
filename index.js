a17f8957e4b14040b8eb5c6cd0ec25e101f44b0c
Mozahid Izhar <mozahidizhar54@gmail.com>
2026-03-06 12:07:27 +0530

    My Family Information

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const myName = "Mozahid Izhar"
 const mySchool = "Daniel Public School"
 const myAge = 21
 const myHigherEducation = "Graduate"
+
+
+const Family = () => {
+  const fatherName = " Izharul Haque Ansari"
+  const motherName = " Rukhsana Khatoon"
+  const sisterName = " Reshma Izhar"
+
+  console.log(fatherName, motherName, sisterName);
+}
+
+Family()

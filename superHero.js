150efc4489a702a14f5fb06d0a89ca459c36a3dc
Mozahid <mozahidizhar54@gmail.com>
2026-03-06 15:29:30 +0530

    Created the array of 20 heros

---
+++ superHero.js
@@ -0,0 +1,24 @@
+const superHeroes = [
+  "Superman",
+  "Batman",
+  "Spider-Man",
+  "Iron Man",
+  "Captain America",
+  "Thor",
+  "Hulk",
+  "Black Panther",
+  "Doctor Strange",
+  "Wonder Woman",
+  "Flash",
+  "Aquaman",
+  "Green Lantern",
+  "Scarlet Witch",
+  "Vision",
+  "Black Widow",
+  "Hawkeye",
+  "Ant-Man",
+  "Wolverine",
+  "Deadpool"
+];
+
+console.log(superHeroes);
